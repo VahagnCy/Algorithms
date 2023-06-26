51467b3a70cf656399cf80265af080b8f81a3fc7
vhovvyan <vahagnhovvyan@gmail.com>
2023-06-26 15:31:17 +0400

    datastructures

---
+++ src/NextGreaterElement.java
@@ -0,0 +1,40 @@
+import java.util.Stack;
+
+public class NextGreaterElement {
+    public static void main(String[] args) {
+        printNextGreaterElement(new int[] {16, 7, 2, 15});
+    }
+
+    public static void printNextGreaterElement(int[] arr) {
+        if (arr.length == 0) {
+            System.out.println();
+            return;
+        }
+
+        Stack<Integer> stack = new Stack<>();
+        stack.push(arr[0]);
+
+        for (int i = 0; i < arr.length; i++) {
+            int next = arr[i];
+            if (!stack.isEmpty()) {
+                int popped = stack.pop();
+                while (popped < next) {
+                    System.out.println(popped + "-->" + next);
+                    if (stack.isEmpty()) {
+                        break;
+                    }
+                    popped = stack.pop();
+                }
+
+                if (popped > next) {
+                    stack.push(popped);
+                }
+            }
+            stack.push(next);
+        }
+
+        while (!stack.isEmpty()) {
+            System.out.println(stack.pop() + " -->" + -1);
+        }
+    }
+}
---
+++ src/QueueStandards.java
@@ -0,0 +1,21 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class QueueStandards {
+    public static void main(String[] args) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(2);
+        queue.add(3);
+        queue.add(19);
+        queue.add(20);
+
+        int removeItem = queue.remove();
+        System.out.println(removeItem);
+        System.out.println(queue.remove());
+        System.out.println(queue.peek());
+
+        while (!queue.isEmpty()) {
+            System.out.println(queue.remove());
+        }
+    }
+}
