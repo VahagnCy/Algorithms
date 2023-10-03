792a2205676060d00aa5fa4d8e86f3335b9d323a
HovvyanV <coreezteam@gmail.com>
2023-10-03 20:37:17 +0400

    serviceTitan Task

--- src/ReverseString.java
+++ src/ReverseString.java
@@ -18,7 +18,6 @@ public class ReverseString {
         for (int i = s.length() - 1; i >= 0; i--) {
             reversed.append(s.charAt(i));
         }
-
         return reversed.toString();
     }
 //////////////////////////////////////////////////////////////////////////////////////////////////
